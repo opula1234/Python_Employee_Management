987d7413d7f0b69ccd7df52a6ec8e7e1709f520a
opula1234 <balajioropula@gmail.com>
2025-06-25 21:58:31 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+
+app = FastAPI(title="FastAPI Development",
+              summary="Best API Development for python easy to learn and production ready...",
+              version="1")
+
+
+@app.get("/")
+def welcome_greet():
+    return {"message": "Welcome to FastAPI Development..."}
