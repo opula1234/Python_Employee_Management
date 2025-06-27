24c936ccb69009ed6c890e660ae71a62de866bbd
opula1234 <balajioropula@gmail.com>
2025-06-27 07:40:58 +0530

    Fix: added doc string and function response

--- app/main.py
+++ app/main.py
@@ -1,15 +1,21 @@
+"""Main module for FastAPI Development application."""
+
 from fastapi import FastAPI
 
-app = FastAPI(title="FastAPI Development",
-              summary="Best API Development for python easy to learn and production ready...",
-              version="1")
+app = FastAPI(
+    title="FastAPI Development",
+    summary="Best API Development for python easy to learn and " "production ready...",
+    version="1",
+)
 
 
 @app.get("/")
 def welcome_greet():
+    """Return a welcome message for the FastAPI application."""
     return {"message": "Welcome to FastAPI Development..."}
 
 
 @app.get("/info")
 def show_information():
+    """Return information about the application."""
     return {"message": "This is the information page"}
